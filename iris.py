423e785fd6c94398eeb64cbad7771f71c260636b
Sinead Frawley <sinead.frawley@gmail.com>
2019-04-06 23:40:54 +0100

    change in iris.py added classes and main method to project

--- iris.py
+++ iris.py
@@ -1,145 +1,100 @@
-# Load libraries
-import pandas as pd
-from pandas.plotting import scatter_matrix
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 20:42:06 2019
+
+@author: SineadF
+"""
+
 import matplotlib.pyplot as plt
 from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 import seaborn as sns
-import urllib.request
-
-#  Load Dataset
-iris = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-
-data = urllib.request.urlopen(iris)
-df = pd.read_csv(data, sep=',')
-attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
-df.columns = attributes
-# Shape
-print(df.shape)
-
-
-# peak at the Data
-print(df.head(20))
-
-
-# Summary stattistics
-
-print(df.describe())
-
-print(df.groupby('class').size())
-
-# box and whisker plots
-df.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.savefig('boxplot.png') 
-plt.show()
-
-# histograms
-df.hist()
-plt.savefig('histrogram.png') 
-plt.show()
+import dataAnalyticsProjectMachineLearning as dap
 
 
 
-# scatter plot matrix
-scatter_matrix(df)
-plt.savefig('scatter_matrix.png') 
-plt.show()
-
-
-# Split-out validation dataset
-array = df.values
-X = array[:,0:4]
-Y = array[:,4]
-validation_size = 0.20
-seed = 7
-scoring = 'accuracy'
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
 
 
-# Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
-# evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
 
 
 
-# Compare Algorithms
-fig = plt.figure()
-fig.suptitle('Algorithm Comparison')
-ax = fig.add_subplot(111)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
+
+def main():
+    iris = dap.MahcineLeanringClass()
+    iris.url='http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+    iris.attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+    df = iris.loadDataSet()
+    # Shape
+    print(' The shape of the iris data set')
+    print(df.shape)
+    print(' *************************************************')
+    # peak at the Data
+    print(' Peak at the Data')
+    print(df.head(20))
+    print(' *************************************************')
+    print(' Summary stattistics')
+    # Summary stattistics
+    print(df.describe())
+    print(' *************************************************')
+    print('Compute group sizes')
+    print(df.groupby('class').size())
+    print(' *************************************************')
+    print('Box Plots')
+    iris.getBoxPlot('boxplot.png')
+    print(' *************************************************')
+    print('Histograms')
+    iris.getHistrogram('histrogram.png')
+    print(' *************************************************')
+    print('Scatter Matrix ')
+    iris.getScatterMatrix('scatter_matrix.png')
+    print(' *************************************************')
+    print('Split-out validation dataset ')
+    array = df.values
+    X = array[:,0:4]
+    Y = array[:,4]
+    validation_size = 0.20
+    seed = 7
+    scoring = 'accuracy'
+    X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+    print(' *************************************************')
+    print('Spot Check Algorithms ')
+    models,results,names=iris.checkAlgorithmModels(seed,X_train,Y_train,scoring)
+    print(' *************************************************')
+    print('Compare Algorithms ')
+    iris.compareAlgorithms(names,results)
+    print(' *************************************************')
+    print('Make predictions on validation dataset Fit the classifier to the data ')
+    iris.getknnClassifierPredictions(X_train, X_validation, Y_train, Y_validation)
+    # example of training a final classification model
+    # generate 2d classification dataset
+    # fit final model
+    model = LogisticRegression()
+    model.fit(X, Y)
+    ## Exploratory data analysis
+    ## I be working with the iris dataset. My goal was  
+    ## be to predict species('Iris-versicolor','Iris-virginica','Iris-setosa') based on "sepal_length", "sepal_width", "petal_length", "petal_width"
+    plt.figure()
+    sns.countplot(x='sepal_length', hue='class', data=df, palette='RdBu')
+    plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
+    plt.show()
+    plt.figure()
+    sns.countplot(x='sepal_width', hue='class', data=df, palette='RdBu')
+    plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
+    plt.show()
+    plt.figure()
+    sns.countplot(x='petal_length', hue='class', data=df, palette='RdBu')
+    plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
+    plt.show()
+    
+    plt.figure()
+    sns.countplot(x='petal_width', hue='class', data=df, palette='RdBu')
+    plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
+    plt.show()
+        
 
 
 
-# Make predictions on validation dataset
-knn = KNeighborsClassifier()
-# Fit the classifier to the data
-knn.fit(X_train, Y_train)
-# Predict the labels of the training data
-predictions = knn.predict(X_train)
-print("Prediction: {}".format(predictions))
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
 
-
-
-# example of training a final classification model
-# generate 2d classification dataset
-# fit final model
-model = LogisticRegression()
-model.fit(X, Y)
-
-
-## Exploratory data analysis
-
-
-## I be working with the iris dataset. My goal was  
-## be to predict species('Iris-versicolor','Iris-virginica','Iris-setosa') based on "sepal_length", "sepal_width", "petal_length", "petal_width"
-
-
-plt.figure()
-sns.countplot(x='sepal_length', hue='class', data=df, palette='RdBu')
-plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
-plt.show()
-
-plt.figure()
-sns.countplot(x='sepal_width', hue='class', data=df, palette='RdBu')
-plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
-plt.show()
-
-
-plt.figure()
-sns.countplot(x='petal_length', hue='class', data=df, palette='RdBu')
-plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
-plt.show()
-
-plt.figure()
-sns.countplot(x='petal_width', hue='class', data=df, palette='RdBu')
-plt.xticks([0,1,2], ['Iris-versicolor','Iris-virginica','Iris-setosa'])
-plt.show()
+if __name__ == "__main__":
+    main()
